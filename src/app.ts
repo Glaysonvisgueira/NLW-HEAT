3c16785001fe795b84855290fd927738a74352d1
Glaysonvisgueira <glaysonwow@gmail.com>
2021-10-19 14:48:14 -0300

    Integração com Prima ORM

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import "dotenv/config";
 import express from 'express';
 
-const app = express();
+import { router } from './routes';
 
+const app = express();
+app.use(express.json());
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
 
 
 app.get("/signin/callback", (request, response) => {
@@ -11,7 +19,3 @@ app.get("/signin/callback", (request, response) => {
 }) 
 
 app.listen(4000, () => console.log(`Servidor rodando na porta: 4000.`))
-
-app.get("/github", (request, response) => {
-    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
